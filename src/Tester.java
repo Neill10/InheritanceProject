b5fd565c4a30f77e31e5f38405371b8ed9906e81
Neill10 <neill10@nycstudents.net>
2022-03-16 22:15:34 -0400

    fixed some stuff about install-drive

--- src/Tester.java
+++ src/Tester.java
@@ -2,12 +2,17 @@ import java.util.*;
 
 public class Tester {
     public static void main(String[] args) {
-        Logic logic;
+        Logic logic = new Logic();
         Scanner s = new Scanner(System.in);
-        System.out.println("Welcome to the LVM system! Enter your commands:\n\n");
+        System.out.println("Welcome to the LVM system! Enter your commands:\n");
         System.out.println("cmd#: ");
         String userChoice = s.nextLine();
         logic.choices(userChoice);
+
+        logic.getPDList();
+
+
+
 
 
 
